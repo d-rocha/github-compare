884d0ba4270ecff30b5ef190c5d0926218703881
davi.rocha <davi.rocha@intercase.com.br>
2019-08-21 18:23:41 -0300

    config loding state

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -14,11 +14,15 @@ export default class Search extends Component {
       repositoryInput: '',
       repositories: [],
       error: false,
+      loading: false
     }
   }
 
   handleRepositoryAdd = async (event) => {
     event.preventDefault();
+
+    this.setState({ loading: true })
+
     try {
       const { data: res } = await api.get(`/repos/${this.state.repositoryInput}`);
       res.lastCommit = moment(res.pushed_at).fromNow();
@@ -28,21 +32,23 @@ export default class Search extends Component {
         repositories: [...this.state.repositories, res],
         error: false
       });
+
     } catch (err) {
       this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
     }
   };
 
   render() {
     return (
-
       <ContainerBox>
         <img src={logo} alt="GitHub Compare" />
         <Form error={this.state.error} onSubmit={this.handleRepositoryAdd}>
           <input type="text" placeholder="usuário/repositório"
             value={this.state.repositoryInput}
             onChange={event => this.setState({ repositoryInput: event.target.value })} />
-          <button type="submit">+</button>
+          <button type="submit">{this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : '+'}</button>
         </Form>
         <RepositoriesList repositories={this.state.repositories} />
       </ContainerBox>
